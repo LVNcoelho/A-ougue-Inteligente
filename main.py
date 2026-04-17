1f80125635160c909c75c9d27e96df7ea7491941
Conecta TI <pamelacoelho117@gmail.com>
2026-04-17 08:11:36 -0300

    Refactor main.py for better structure and clarity

--- main.py
+++ main.py
@@ -108,5 +108,5 @@ async def painel_cliente(request: Request):
         "estoque": res.data
     })
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+#if __name__ == "__main__":
+  #  uvicorn.run(app, host="0.0.0.0", port=8000)
